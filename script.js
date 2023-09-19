ff95a4cf3d514fd3dd3ed970aa6f39298081380e
Subham jain <134865513+Subham7769@users.noreply.github.com>
2023-09-19 11:50:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
-function myFunction(){
-    document.getElementById("paragraph").innerHTML = "DOM load success";
-}   
+document.addEventListener("DOMContentLoaded",()=>{
+document.body.innerText = "DOM load success";
+});
